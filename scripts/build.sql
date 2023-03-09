d3ea6456465854dab0635cf69c965b9bf2d23c0f
Daniel M. Mwale <danny@arcariusmexen.com>
2023-03-09 17:31:19 +0200

    build script details

---
+++ scripts/build.sql
@@ -0,0 +1,11 @@
+/*
+Author: Daniel M. Mwale
+Email: danny@arcariusmexen.com
+Date: 2023-03-09 Thursday
+Description: Build script for IaRS-SE. It contains CREATE statements for a fully
+             relational schema to store invoicing and receipting data
+VERSION: 0.1.0
+*/
+CREATE TABLE invoice (
+
+)
