059fc257a559d3ff8833b5e2b3cc5998bd1afa4d
Daniel M. Mwale <danny@arcariusmexen.com>
2023-03-11 18:35:38 +0200

    Check if a table exists before creating one. Ignore if it does

--- scripts/build.sql
+++ scripts/build.sql
@@ -6,7 +6,9 @@ Description: Build script for IaRS-SE. It contains CREATE statements for a fully
              relational schema to store invoicing and receipting data
 VERSION: 0.1.0
 */
-CREATE TABLE invoice (
+
+
+CREATE TABLE IF NOT EXISTS invoice (
     id bigserial,
     invoice_number text,
     total_amount money NOT NULL,
