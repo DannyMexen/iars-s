75389ea9b1f3df627056fc92aaf82d18f5712719
Daniel M. Mwale <danny@arcariusmexen.com>
2023-03-13 11:52:57 +0200

    fixed several syntax errors

--- scripts/build.sql
+++ scripts/build.sql
@@ -92,28 +92,28 @@ CREATE TABLE IF NOT EXISTS simple.user_role (
 CREATE TABLE IF NOT EXISTS simple.user_department (
     id bigserial PRIMARY KEY,
     name text NOT NULL UNIQUE,
-    description NOT NULL UNIQUE
+    description text NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS simple.user_account_status (
     id bigserial PRIMARY KEY,
     name text DEFAULT 'ACTIVE' UNIQUE,
-    description NOT NULL UNIQUE
+    description text NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS simple.user (
     id bigserial,
-    user_name text, 
+    username text, 
     password_hash text NOT NULL,
     user_account_status_id bigint REFERENCES simple.user_account_status (id),
     department_id bigint NOT NULL REFERENCES simple.user_department (id),
-    employee_number text NOT NULL,
+    employee_number text NOT NULL UNIQUE,
     user_role_id bigint NOT NULL REFERENCES simple.user_role (id),
     first_name text NOT NULL,
     last_name text NOT NULL,
     phone_number text NOT NULL UNIQUE,
 
-    PRIMARY KEY (id, username)
+    PRIMARY KEY (id, username, employee_number)
 );
 
 -- C.   LOG AND EVENTS
