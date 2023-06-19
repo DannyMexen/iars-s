3f74cdead4d2ce12d0c33658f41660b5fd0686cf
Daniel M. Mwale <danny@arcariusmexen.com>
2023-06-19 09:32:57 +0200

    Add user

--- simple/aya/models.py
+++ simple/aya/models.py
@@ -2,16 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+
 # A. Standalone tables - no relationships
 # Notifications
 class Notification(models.Model):
-    name = models.CharField(unique=True,max_length=100,null=False)
-    description = models.TextField(max_length=300,null=False)
+    name = models.CharField(unique=True, max_length=100, null=False)
+    description = models.TextField(max_length=300, null=False)
+
 
 # Change Reasons
 class ChangeReason(models.Model):
-    name = models.CharField(unique=True,max_length=100,null=False)
-    description = models.TextField(max_length=300,null=False)
+    name = models.CharField(unique=True, max_length=100, null=False)
+    description = models.TextField(max_length=300, null=False)
+
 
 # B. Users
 class UserRole(models.Model):
@@ -27,13 +30,14 @@ class UserRole(models.Model):
         choices=ROLE_CHOICES,
     )
 
-    description = models.TextField(max_length=300,null=False)
+    description = models.TextField(max_length=300, null=False)
+
 
 class UserAccountStatus(models.Model):
-    ACTIVE = "AC" # Enabled and in use
-    INACTIVE = "IA" # Not in use but accessible
-    DISABLED = "DI" # Requires activation to use
-    ARCHIVED = "AR" # Completely unusable and cannot be activated
+    ACTIVE = "AC"  # Enabled and in use
+    INACTIVE = "IA"  # Not in use but accessible
+    DISABLED = "DI"  # Requires activation to use
+    ARCHIVED = "AR"  # Completely unusable and cannot be activated
     ACCOUNT_STATUS_CHOICES = [
         (ACTIVE, "Active"),
         (INACTIVE, "Inactive"),
@@ -45,5 +49,16 @@ class UserAccountStatus(models.Model):
         default=ACTIVE,
         choices=ACCOUNT_STATUS_CHOICES,
     )
-    description = models.TextField(max_length=300,null=False)
+    description = models.TextField(max_length=300, null=False)
+
+
+class User(models.Model):
+    login_name = models.CharField(max_length=100, unique=True)
+    password_hash = models.TextField(max_length=500, unique=True)
+    user_account_status_id = models.ForeignKey(UserAccountStatus, on_delete=models.RESTRICT)
+    user_role_id = models.ForeignKey(UserRole, on_delete=models.RESTRICT)
+    first_name = models.CharField(max_length=100) 
+    last_name = models.CharField(max_length=100) 
+    phone_number = models.CharField(max_length=13, unique=True) # TODO: PhoneNumberField
+    email = models.EmailField(max_length=254, unique=True)
 
