4fcced0b5265672cbe4fac918c535ba4359dc26e
Daniel M. Mwale <danny@arcariusmexen.com>
2023-06-19 08:48:41 +0200

    Add user account status

--- simple/aya/models.py
+++ simple/aya/models.py
@@ -13,7 +13,7 @@ class ChangeReason(models.Model):
     name = models.CharField(unique=True,max_length=100,null=False)
     description = models.TextField(max_length=300,null=False)
 
-# B. User Roles
+# B. Users
 class UserRole(models.Model):
     ADMINISTRATOR = "AD"
     STANDARD = "ST"
@@ -21,3 +21,29 @@ class UserRole(models.Model):
         (ADMINISTRATOR, "Administrator"),
         (STANDARD, "Standard"),
     ]
+    name = models.CharField(
+        unique=True,
+        default=STANDARD,
+        choices=ROLE_CHOICES,
+    )
+
+    description = models.TextField(max_length=300,null=False)
+
+class UserAccountStatus(models.Model):
+    ACTIVE = "AC" # Enabled and in use
+    INACTIVE = "IA" # Not in use but accessible
+    DISABLED = "DI" # Requires activation to use
+    ARCHIVED = "AR" # Completely unusable and cannot be activated
+    ACCOUNT_STATUS_CHOICES = [
+        (ACTIVE, "Active"),
+        (INACTIVE, "Inactive"),
+        (DISABLED, "Disabled"),
+        (ARCHIVED, "Archived"),
+    ]
+    name = models.CharField(
+        unique=True,
+        default=ACTIVE,
+        choices=ACCOUNT_STATUS_CHOICES,
+    )
+    description = models.TextField(max_length=300,null=False)
+
