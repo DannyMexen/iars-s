a8f139abff85dc43fed2aa0382a4c902468671b2
Daniel M. Mwale <danny@arcariusmexen.com>
2023-06-19 07:31:05 +0200

    Add notification model

--- simple/aya/models.py
+++ simple/aya/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# Notifications
+class Notification(models.Model):
+    name = models.CharField(unique=True,max_length=100,null=False)
+    description = models.TextField(max_length=300,null=False)
+
+# Change Reasons
+class ChangeReason(models.Model):
+    name = models.CharField(unique=True,max_length=100,null=False)
+    description = models.TextField(max_length=300,null=False)
