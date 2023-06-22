56b27697896dda561853622de3e10853168f1e8e
Daniel Mwale <danny@arcariusmexen.com>
2023-06-22 16:50:20 +0200

    # This is a combination of 11 commits. # This is the 1st commit message:
    Fix merge conflict
    
    # This is the commit message #2:
    
    Add home screen view
    
    # This is the commit message #3:
    
    Create aya URLconf
    
    # This is the commit message #4:
    
    Point root URLconf at aya.urls module
    
    # This is the commit message #5:
    
    __pycache__
    
    # This is the commit message #6:
    
    Configure database
    
    # This is the commit message #7:
    
    Change timezone to local
    
    # This is the commit message #8:
    
    Migrate initial apps
    
    # This is the commit message #9:
    
    Add notification model
    
    # This is the commit message #10:
    
    Add user role
    
    # This is the commit message #11:
    
    Include app to project

--- simple/aya/models.py
+++ simple/aya/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+# A. Standalone tables - no relationships
+# Notifications
+class Notification(models.Model):
+    name = models.CharField(unique=True,max_length=100,null=False)
+    description = models.TextField(max_length=300,null=False)
+
+# Change Reasons
+class ChangeReason(models.Model):
+    name = models.CharField(unique=True,max_length=100,null=False)
+    description = models.TextField(max_length=300,null=False)
+
+# B. User Roles
+class UserRole(models.Model):
+    ADMINISTRATOR = "AD"
+    STANDARD = "ST"
+    ROLE_CHOICES = [
+        (ADMINISTRATOR, "Administrator"),
+        (STANDARD, "Standard"),
+    ]
