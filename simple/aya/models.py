4c3a7f6af448423ab68efa51e58a1eb9a0c00258
Daniel Mwale <danny@arcariusmexen.com>
2023-06-22 16:50:43 +0200

    # This is a combination of 15 commits. # This is the 1st commit message:
    Fix merge conflict
    
    # This is the commit message #2:
    
    Add home screen view
    
    # This is the commit message #3:
    
    Create aya URLconf
    
    # This is the commit message #4:
    
    Point root URLconf at aya.urls module
    
    # This is the commit message #5:
    
    __pycache__
    
    # This is the commit message #6:
    
    Configure database
    
    # This is the commit message #7:
    
    Change timezone to local
    
    # This is the commit message #8:
    
    Migrate initial apps
    
    # This is the commit message #9:
    
    Add notification model
    
    # This is the commit message #10:
    
    Add user role
    
    # This is the commit message #11:
    
    Include app to project
    
    # This is the commit message #12:
    
    Make migration - standalone tables
    
    # This is the commit message #13:
    
    Add user account status
    
    # This is the commit message #14:
    
    Add user
    
    # This is the commit message #15:
    
    Fix field default value requirement

--- simple/aya/models.py
+++ simple/aya/models.py
@@ -2,18 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+
 # A. Standalone tables - no relationships
 # Notifications
 class Notification(models.Model):
-    name = models.CharField(unique=True,max_length=100,null=False)
-    description = models.TextField(max_length=300,null=False)
+    name = models.CharField(unique=True, max_length=100)
+    description = models.TextField(max_length=300)
+
 
 # Change Reasons
 class ChangeReason(models.Model):
-    name = models.CharField(unique=True,max_length=100,null=False)
-    description = models.TextField(max_length=300,null=False)
+    name = models.CharField(unique=True, max_length=100)
+    description = models.TextField(max_length=300)
 
-# B. User Roles
+
+# B. Users
 class UserRole(models.Model):
     ADMINISTRATOR = "AD"
     STANDARD = "ST"
@@ -21,3 +24,43 @@ class UserRole(models.Model):
         (ADMINISTRATOR, "Administrator"),
         (STANDARD, "Standard"),
     ]
+    name = models.CharField(
+        unique=True,
+        default=STANDARD,
+        choices=ROLE_CHOICES,
+    )
+
+    STD_DESCRIPTION = "Standard user - core system access privileges."
+    # ADMIN_DESCRIPTION = "Administrator  - elevated system access privileges."
+    description = models.TextField(max_length=300, default=STD_DESCRIPTION)
+
+
+class UserAccountStatus(models.Model):
+    ACTIVE = "AC"  # Enabled and in use
+    INACTIVE = "IA"  # Not in use but accessible
+    DISABLED = "DI"  # Requires activation to use
+    ARCHIVED = "AR"  # Completely unusable and cannot be activated
+    ACCOUNT_STATUS_CHOICES = [
+        (ACTIVE, "Active"),
+        (INACTIVE, "Inactive"),
+        (DISABLED, "Disabled"),
+        (ARCHIVED, "Archived"),
+    ]
+    name = models.CharField(
+        unique=True,
+        default=ACTIVE,
+        choices=ACCOUNT_STATUS_CHOICES,
+    )
+    description = models.TextField(max_length=300)
+
+
+class User(models.Model):
+    login_name = models.CharField(max_length=100, unique=True)
+    password_hash = models.TextField(max_length=500, unique=True)
+    user_account_status_id = models.ForeignKey(UserAccountStatus, on_delete=models.RESTRICT)
+    user_role_id = models.ForeignKey(UserRole, on_delete=models.RESTRICT)
+    first_name = models.CharField(max_length=100) 
+    last_name = models.CharField(max_length=100) 
+    phone_number = models.CharField(max_length=13, unique=True) # TODO: PhoneNumberField
+    email = models.EmailField(max_length=254, unique=True)
+
